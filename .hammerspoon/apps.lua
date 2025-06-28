23bdc04aa210ea94d228fbec29ed76787ca2bf1a
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-06-28 20:14:48 +0200

    feat(hs): add default size to scratch apps

--- .hammerspoon/apps.lua
+++ .hammerspoon/apps.lua
@@ -17,23 +17,29 @@ for key, app in pairs(apps) do
 		if runningApp and runningApp:isFrontmost() then
 			runningApp:hide()
 		else
-			local frame = hs.screen.mainScreen():frame()
-			local window = runningApp:mainWindow()
-			local width = math.max(frame.w / 2, 1500)
-			local height = math.max(frame.h / 2, 1000)
-			window:setFrame({
-				w = width,
-				h = height,
-				x = frame.w / 2 - width / 2,
-				y = frame.h / 2 - height / 2,
-			})
 			hs.application.open(app)
 		end
 	end)
 end
 
 M.hideAppOnDeactivate = function(appName, eventType, appObject)
-	if eventType == hs.application.watcher.deactivated then
+	if eventType == hs.application.watcher.activated then
+		for _, app in pairs(apps) do
+			if appName == app then
+				local runningApp = hs.application(appName)
+				local frame = hs.screen.mainScreen():frame()
+				local window = runningApp:mainWindow()
+				local width = math.max(frame.w / 2, 1500)
+				local height = math.max(frame.h / 2, 1000)
+				window:setFrame({
+					w = width,
+					h = height,
+					x = frame.w / 2 - width / 2,
+					y = frame.h / 2 - height / 2,
+				})
+			end
+		end
+	elseif eventType == hs.application.watcher.deactivated then
 		for _, app in pairs(apps) do
 			if appName == app then
 				appObject:hide()
