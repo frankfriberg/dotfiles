4fcba9cb6cedab1c504407b0021f63eb34d0015f
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-07-23 22:47:32 +0200

    chore: add Brewfile

--- install.sh
+++ install.sh
@@ -22,16 +22,16 @@ fi
 # Sketchybar
 info "Installing Sketchybar Lua..."
 (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+brew bundle install
 
-install_packages
 setup_osx
 setup_git
 
 # Set Wezterm as terminal
-tempfile=$(mktemp) \
-  && curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo \
-  && tic -x -o ~/.terminfo $tempfile \
-  && rm $tempfile
+tempfile=$(mktemp) &&
+  curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo &&
+  tic -x -o ~/.terminfo $tempfile &&
+  rm $tempfile
 
 stow .
 
