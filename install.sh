902a6f98111582923897d6ded4f626a7c362324f
Frank Friberg <frankfriberg@users.noreply.github.com>
2025-08-05 22:46:02 +0200

    chore: remove outdated install script

--- install.sh
+++ install.sh
@@ -5,31 +5,17 @@ set -o nounset
 set -o pipefail
 
 . scripts/utils.sh
-. scripts/brew.sh
-. scripts/packages.sh
 . scripts/osx.sh
-. scripts/git.sh
 
 if ! command -v brew &>/dev/null; then
   info "Installing brew..."
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if [ ! -d ~/Development ]; then
-  mkdir ~/Development
+if [ ! -d ~/projects ]; then
+  mkdir ~/projects
 fi
 
-brew bundle install
-
-setup_osx
-setup_git
-
-# Set Wezterm as terminal
-tempfile=$(mktemp) &&
-  curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo &&
-  tic -x -o ~/.terminfo $tempfile &&
-  rm $tempfile
 
 stow .
 
-nvim --headless "+Lazy! install" +qall
