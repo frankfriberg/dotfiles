c9dcbf60119fb13d7eb79cdce2d6d6d85fc5c642
Frank Friberg <frankfriberg@users.noreply.github.com>
2026-01-14 08:49:40 +0100

    fix: add brew to path after install

--- install.sh
+++ install.sh
@@ -4,17 +4,24 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if ! command -v brew &>/dev/null; then
+	info "Installing brew..."
+	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+	# Add Homebrew to PATH for the current session
+	if [[ $(uname -m) == 'arm64' ]]; then
+		eval "$(/opt/homebrew/bin/brew shellenv)"
+	else
+		eval "$(/usr/local/bin/brew shellenv)"
+	fi
+fi
+
 . scripts/utils.sh
 . scripts/osx.sh
 . scripts/kanata.sh
 
-if ! command -v brew &>/dev/null; then
-  info "Installing brew..."
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
 if [ ! -d ~/projects ]; then
-  mkdir ~/projects
+	mkdir ~/projects
 fi
 
 brew bundle
